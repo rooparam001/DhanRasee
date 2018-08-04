5db93adb58de1e991ca706ae0eb5f89d0e37bac5
rooparam001 <40693448+rooparam001@users.noreply.github.com>
2018-08-04 15:49:55 +0530

    Select loan category added.

--- app/src/main/java/com/viet/rooparam/dhanrasee/LoanCategoryActivity.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/LoanCategoryActivity.java
@@ -3,18 +3,138 @@ package com.viet.rooparam.dhanrasee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LoanCategoryActivity extends AppCompatActivity {
 
-    ExpandableListView expandableListView;
-    
+    LoanCategoryAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_category);
+
+        expListView = (ExpandableListView) findViewById(R.id.expandable_loan_list);
+        toolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(R.string.select_loan);
+        prepareListData();
+
+        listAdapter = new LoanCategoryAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        expListView.setGroupIndicator(null);
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                Toast.makeText(getApplicationContext(), "Group Clicked " + listDataHeader.get(groupPosition), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add(getString(R.string.personal_loan));
+        listDataHeader.add(getString(R.string.business_loan));
+        listDataHeader.add(getString(R.string.daily_loan));
+        listDataHeader.add(getString(R.string.home));
+        listDataHeader.add(getString(R.string.vehicle_loan));
+        listDataHeader.add(getString(R.string.gold_loan));
+
+        List<String> personalLoan = new ArrayList<>();
+        List<String> businessLoan = new ArrayList<>();
+        List<String> dailyLoan = new ArrayList<>();
+        List<String> goldLoan = new ArrayList<>();
+
+        // Adding child data
+        List<String> home = new ArrayList<String>();
+        home.add(getString(R.string.new_purchase));
+        home.add(getString(R.string.construction));
+        home.add(getString(R.string.mortgage));
+
+        List<String> vehicle = new ArrayList<String>();
+        vehicle.add(getString(R.string.two_wheeler));
+        vehicle.add(getString(R.string.four_wheeler));
+        vehicle.add(getString(R.string.commercial_new));
+        vehicle.add(getString(R.string.two_wheeler_ref));
+        vehicle.add(getString(R.string.four_wheeler_ref));
+        vehicle.add(getString(R.string.commercial_new_ref));
+
+        listDataChild.put(listDataHeader.get(0), personalLoan); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), businessLoan); // Header, Child data
+        listDataChild.put(listDataHeader.get(2), dailyLoan); // Header, Child data
+        listDataChild.put(listDataHeader.get(3), home);
+        listDataChild.put(listDataHeader.get(4), vehicle);
+        listDataChild.put(listDataHeader.get(5), goldLoan);
 
     }
 }
