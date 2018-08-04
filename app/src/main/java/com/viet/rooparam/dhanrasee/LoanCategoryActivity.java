d0c7597c2144915b31e75a18161f6b0f906e2686
rooparam001 <40693448+rooparam001@users.noreply.github.com>
2018-08-04 13:57:34 +0530

    Total App

--- app/src/main/java/com/viet/rooparam/dhanrasee/LoanCategoryActivity.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/LoanCategoryActivity.java
@@ -3,17 +3,18 @@ package com.viet.rooparam.dhanrasee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 public class LoanCategoryActivity extends AppCompatActivity {
 
-
+    ExpandableListView expandableListView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_category);
-
 
     }
 }
