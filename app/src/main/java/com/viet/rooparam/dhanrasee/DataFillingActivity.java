7cffba93e77796a5db9fc911c7605e39e23ae94d
rooparam001 <40693448+rooparam001@users.noreply.github.com>
2018-08-05 22:19:53 +0530

    personal detail added.

--- app/src/main/java/com/viet/rooparam/dhanrasee/DataFillingActivity.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/DataFillingActivity.java
@@ -2,12 +2,24 @@ package com.viet.rooparam.dhanrasee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 
 public class DataFillingActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_filling);
+
+        frameLayout = findViewById(R.id.data_frame);
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(R.string.personal_detail);
+
     }
 }
--- app/src/main/java/com/viet/rooparam/dhanrasee/PersonalDetailFragment.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/PersonalDetailFragment.java
@@ -4,16 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class PersonalDetailFragment extends Fragment {
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_personal_detail, container, false);
 
         return view;
