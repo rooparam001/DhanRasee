e7b46bd54528efc254728d2ecc179288c9b5ecc4
rooparam001 <40693448+rooparam001@users.noreply.github.com>
2018-08-06 00:50:41 +0530

    Intent added.

--- app/src/main/java/com/viet/rooparam/dhanrasee/OfficialDetailFragment.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/OfficialDetailFragment.java
@@ -4,22 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class OfficialDetailFragment extends Fragment {
 
+    Button next_btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_official_detail, container, false);
+       next_btn = view.findViewById(R.id.official_next_button);
 
+       next_btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
 
+               ImageUploadFragment imageUploadFragment = new ImageUploadFragment();
+               FragmentTransaction ftImage= getFragmentManager().beginTransaction();
+               ftImage.replace(R.id.data_frame,imageUploadFragment);
+               ftImage.commit();
 
+           }
+       });
         return view;
     }
 
--- app/src/main/java/com/viet/rooparam/dhanrasee/PersonalDetailFragment.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/PersonalDetailFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +24,10 @@ public class PersonalDetailFragment extends Fragment {
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                OfficialDetailFragment officialDetailFragment = new OfficialDetailFragment();
+                FragmentTransaction ftOfficial = getFragmentManager().beginTransaction();
+                ftOfficial.replace(R.id.data_frame,officialDetailFragment);
+                ftOfficial.commit();
 
             }
         });
