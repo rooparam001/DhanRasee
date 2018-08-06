b4d5959b0835f35d14b306041650c726771d84c6
Hellblazer10 <theumeshlakhnai@gmail.com>
2018-08-06 13:08:49 +0530

    image activity added.

--- app/src/main/java/com/viet/rooparam/dhanrasee/ImageUploadFragment.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/ImageUploadFragment.java
@@ -1,20 +1,94 @@
 package com.viet.rooparam.dhanrasee;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import static android.support.v4.media.MediaBrowserServiceCompat.RESULT_OK;
 
 
 public class ImageUploadFragment extends Fragment {
+
+    Button front_aadhar_button;
+
+    private static int IMAGE_PICKER_SELECT = 1;
+
+    Context context = getActivity();
+
+    ImageView image;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_image_upload, container, false);
+
+
+        front_aadhar_button = view.findViewById(R.id.front_aadhar_button);
+
+        front_aadhar_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, IMAGE_PICKER_SELECT);
+            }
+        });
+
+
         return view;
+    }
+
+
+    private void startGallery() {
+        Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        cameraIntent.setType("image/*");
+        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, 1000);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = context.getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            image.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            cursor.close();
+        } else {
+            Toast.makeText(getActivity(), "Try Again!!", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+
     }
 }
