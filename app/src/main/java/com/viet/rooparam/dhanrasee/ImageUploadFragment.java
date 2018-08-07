f197a27333fc4d740ba51f039fd33bccedc938d8
Hellblazer10 <theumeshlakhnai@gmail.com>
2018-08-07 20:37:00 +0530

    image activity added.

--- app/src/main/java/com/viet/rooparam/dhanrasee/ImageUploadFragment.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/ImageUploadFragment.java
@@ -2,6 +2,7 @@ package com.viet.rooparam.dhanrasee;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,7 +12,9 @@ import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -22,8 +25,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
+import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 import static android.support.v4.media.MediaBrowserServiceCompat.RESULT_OK;
 
 
@@ -31,11 +37,10 @@ public class ImageUploadFragment extends Fragment {
 
     Button front_aadhar_button;
 
-    private static int IMAGE_PICKER_SELECT = 1;
-
-    Context context = getActivity();
-
     ImageView image;
+
+    public static final int CAMERA_REQUEST = 1888;
+    public static final int MY_CAMERA_PERMISSION_CODE = 1888;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,14 +50,20 @@ public class ImageUploadFragment extends Fragment {
 
         front_aadhar_button = view.findViewById(R.id.front_aadhar_button);
 
+        image = view.findViewById(R.id.image_test);
+
         front_aadhar_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, IMAGE_PICKER_SELECT);
+                if (checkSelfPermission(getActivity(),Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
             }
         });
 
@@ -60,35 +71,31 @@ public class ImageUploadFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new
+                        Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(getActivity(), "camera permission denied", Toast.LENGTH_LONG).show();
+            }
 
-    private void startGallery() {
-        Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        cameraIntent.setType("image/*");
-        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(cameraIntent, 1000);
         }
+
+
     }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    public void onActivityResult ( int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-
-
-        if (data != null) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-            Cursor cursor = context.getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            image.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-            cursor.close();
-        } else {
-            Toast.makeText(getActivity(), "Try Again!!", Toast.LENGTH_SHORT)
-                    .show();
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(photo);
         }
-
-
     }
 }
+
+
