86e6e2536dd15f97c0309896717599d4df56baeb
Hellblazer10 <theumeshlakhani@gmail.com>
2018-08-09 15:49:37 +0530

    PDF service added (Not working properly yet)

---
+++ app/src/main/java/com/viet/rooparam/dhanrasee/GenericFileProvider.java
@@ -0,0 +1,6 @@
+package com.viet.rooparam.dhanrasee;
+
+import android.support.v4.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+}
--- app/src/main/java/com/viet/rooparam/dhanrasee/UserEnteredDataActivity.java
+++ app/src/main/java/com/viet/rooparam/dhanrasee/UserEnteredDataActivity.java
@@ -6,7 +6,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
@@ -40,22 +43,24 @@ public class UserEnteredDataActivity extends AppCompatActivity {
 
         confirm_button = findViewById(R.id.confirm_button);
 
-//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(0, RelativeLayout.LayoutParams.MATCH_PARENT, 1);
-//// 1 here corresponds to the weight to ensure that it is inflated
-//// Depending on how you want to split that screen, you will want to assign a weight to the other view as well
-//        surface.setLayoutParams(params);
-
-        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
-        RelativeLayout root = (RelativeLayout) inflater.inflate(R.layout.activity_user_entered_data, null); //RelativeLayout is root view of my UI(xml) file.
-        root.setDrawingCacheEnabled(true);
-        Bitmap screen= getBitmapFromView(this.getWindow().findViewById(R.id.scroll_data)); // here give id of our root layout (here its my RelativeLayout's id)
 
         confirm_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                File pdfDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "DhanRasee");
+
+                if (!pdfDir.exists())
+                    pdfDir.mkdir();
+
+                File pdfFile = new File(pdfDir, "myPdfFile");
+
+                printData();
+
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                Uri uri = Uri.fromFile(new File(FILE,  "pdfFileName"));
+                Uri uri = FileProvider.getUriForFile(UserEnteredDataActivity.this, UserEnteredDataActivity.this.
+                        getApplicationContext().getPackageName() + ".my.package.name.provider", pdfFile);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 intent.setDataAndType(uri, "application/pdf");
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 startActivity(intent);
@@ -65,38 +70,77 @@ public class UserEnteredDataActivity extends AppCompatActivity {
 
         // add permission in your manifest...
 
-        try
-        {
-            Document document = new Document();
-
-            PdfWriter.getInstance(document, new FileOutputStream(FILE));
-            document.open();
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            screen.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            byte[] byteArray = stream.toByteArray();
-            addImage(document,byteArray);
-            document.close();
-        }
-
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-
-
-
 
     }
 
-    public static Bitmap getBitmapFromView(View view) {
+
+    public void printData() {
+        String state = Environment.getExternalStorageState();
+
+        if (!Environment.getExternalStorageState().equals(state))
+            Toast.makeText(this, "Give Storage Permissions", Toast.LENGTH_LONG).show();
+
+        File pdfDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "DhanRasee");
+
+        if (!pdfDir.exists())
+            pdfDir.mkdir();
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        RelativeLayout root = (RelativeLayout) inflater.inflate(R.layout.activity_user_entered_data, null); //RelativeLayout is root view of my UI(xml) file.
+
+        root.setDrawingCacheEnabled(true);
+
+        RelativeLayout relativeLayout = findViewById(R.id.scroll_data);
+
+        ScrollView view = findViewById(R.id.scroll_view);
+
+        Bitmap screen = getBitmapFromView(view, view.getChildAt(0).getHeight(), view.getChildAt(0).getWidth()); // here give id of our root layout (here its my RelativeLayout's id)
+
+
+        File pdfFile = new File(pdfDir, "myPdfFile.pdf");
+
+
+        try {
+
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            screen.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+            Image image = Image.getInstance(stream.toByteArray());
+
+//            image.setAbsolutePosition(0, 0);
+
+            Document document = new Document();
+
+            PdfWriter.getInstance(document, new FileOutputStream(pdfFile));
+            document.open();
+
+
+            float scale_width = ((document.getPageSize().getWidth() - document.leftMargin()
+                    - document.rightMargin() - 0) / image.getWidth()) * 100;
+
+            float scale_height = ((document.getPageSize().getHeight() - document.leftMargin()
+                    - document.rightMargin() - 0) / image.getHeight()) * 400;
+
+            image.scalePercent(scale_width,scale_height);
+            image.setAlignment(Image.ALIGN_CENTER | Image.ALIGN_TOP);
+
+            document.add(image);
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private Bitmap getBitmapFromView(View view, int height, int width) {
         //Define a bitmap with the same size as the view
-        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Bitmap returnedBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         //Bind a canvas to it
         Canvas canvas = new Canvas(returnedBitmap);
         //Get the view's background
-        Drawable bgDrawable =view.getBackground();
-        if (bgDrawable!=null)
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null)
             //has background drawable, then draw it on the canvas
             bgDrawable.draw(canvas);
         else
@@ -108,31 +152,22 @@ public class UserEnteredDataActivity extends AppCompatActivity {
         return returnedBitmap;
     }
 
-    private static void addImage(Document document,byte[] byteArray)
-    {
+    private static void addImage(Document document, byte[] byteArray) {
         Image image = null;
-        try
-        {
+        try {
             image = Image.getInstance(byteArray);
-        }
-        catch (BadElementException e)
-        {
+        } catch (BadElementException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
-        catch (MalformedURLException e)
-        {
+        } catch (MalformedURLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         // image.scaleAbsolute(150f, 150f);
-        try
-        {
+        try {
             document.add(image);
         } catch (DocumentException e) {
             // TODO Auto-generated catch block
